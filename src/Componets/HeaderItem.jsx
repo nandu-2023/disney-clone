d1bf916b0f29ff47dd7b4342e6ce85ba064e130f
nandu-2023 <152689538+nandu-2023@users.noreply.github.com>
2024-11-14 17:06:12 +0530

    Update HeaderItem.jsx
    'name' is missing in props validation
    'Icon' is missing in props validation

--- src/Componets/HeaderItem.jsx
+++ src/Componets/HeaderItem.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
-function HeaderItem({name,Icon}) {
+function HeaderItem({ name, Icon }) {
   return (
     <div className='text-white flex items-center gap-3
     text-[15px] font-semibold cursor-pointer hover:underline
     underline-offset-8 mb-2'>
-        <Icon/>
+        <Icon />
         <h2 className=''>{name}</h2>
     </div>
   )
 }
 
-export default HeaderItem
+// Prop validation
+HeaderItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  Icon: PropTypes.elementType.isRequired,  // validates that Icon is a component
+}
+
+export default HeaderItem;
