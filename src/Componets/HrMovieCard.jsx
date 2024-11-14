671b86efe7f9931c8e397f6dac2cc63ad8f2e13e
nandu-2023 <152689538+nandu-2023@users.noreply.github.com>
2024-11-14 16:47:52 +0530

    Update HrMovieCard.jsx
    'movie' is missing in props validation
    'movie.backdrop_path' is missing in props validation
    img elements must have an alt prop, either with meaningful text, or an empty string for decorative images.
    'movie.title' is missing in props validation

--- src/Componets/HrMovieCard.jsx
+++ src/Componets/HrMovieCard.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
-const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
+import PropTypes from 'prop-types'
 
-function HrMovieCard({movie}) {
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
+
+function HrMovieCard({ movie }) {
   return (
     <section className='hover:scale-110 transition-all duration-150 ease-in'>
-    <img src={IMAGE_BASE_URL+movie.backdrop_path} 
-    className='w-[110px] md:w-[260px] rounded-lg
-    hover:border-[3px] border-gray-400 cursor-pointer
-    '/>
-    <h2 className='w-[110px] md:w-[260px] text-white
-    mt-2'>{movie.title}</h2>
+      <img 
+        src={IMAGE_BASE_URL + movie.backdrop_path} 
+        alt={movie.title || 'Movie image'}
+        className='w-[110px] md:w-[260px] rounded-lg hover:border-[3px] border-gray-400 cursor-pointer'
+      />
+      <h2 className='w-[110px] md:w-[260px] text-white mt-2'>{movie.title}</h2>
     </section>
   )
+}
+
+// Add propTypes for validation
+HrMovieCard.propTypes = {
+  movie: PropTypes.shape({
+    backdrop_path: PropTypes.string,
+    title: PropTypes.string
+  }).isRequired
 }
 
 export default HrMovieCard
