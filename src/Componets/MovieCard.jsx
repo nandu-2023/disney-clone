495bfeab5774e9ebec177f181f8972474a741cdf
nandu-2023 <152689538+nandu-2023@users.noreply.github.com>
2024-11-14 16:51:20 +0530

    Update MovieCard.jsx
    'movie' is missing in props validation
    A fragment with only one child is redundant.
    'movie.poster_path' is missing in props validation
    img elements must have an alt prop, either with meaningful text, or an empty string for decorative images.

--- src/Componets/MovieCard.jsx
+++ src/Componets/MovieCard.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function MovieCard({movie}) {
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
+
+function MovieCard({ movie }) {
   return (
-    <>
-        <img src={IMAGE_BASE_URL+movie.poster_path} 
-        className='w-[110px] md:w-[200px] rounded-lg
-        hover:border-[3px] border-gray-400 cursor-pointer
-        hover:scale-110 transition-all duration-150 ease-in'/>
-    </>
-  )
+    <img
+      src={IMAGE_BASE_URL + movie.poster_path}
+      alt={movie.title || "Movie poster"}
+      className='w-[110px] md:w-[200px] rounded-lg
+                 hover:border-[3px] border-gray-400 cursor-pointer
+                 hover:scale-110 transition-all duration-150 ease-in'
+    />
+  );
 }
 
-export default MovieCard
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    poster_path: PropTypes.string.isRequired,
+    title: PropTypes.string, // assuming there's a title to display in the alt attribute
+  }).isRequired,
+};
+
+export default MovieCard;
